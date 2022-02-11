bdfbb033219f797d8dbf72c3cd1ac6f15f8c8f01
AlexGlotov <90896181+AlexGlotov@users.noreply.github.com>
2022-02-11 14:39:29 +0300

    Add files via upload

---
+++ sem_2_projict_1/project_1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct bullet {
+
+};
+
+class WarMachine {
+public:
+    WarMachine(string name, double weight, double MaxTime, double MaxSpeed, double armor) {
+        this->name = name;
+        this->weight = weight;
+        this->MaxTime = MaxTime;
+        this->MaxSpeed = MaxSpeed;
+        this->armor = armor;
+        this->speed = speed;
+
+    };
+    ~WarMachine(){
+    
+    };
+    virtual double getWeight() = 0; // получить массу
+    virtual double getTimeWork() = 0; //получить максимальное время автономной работы
+    virtual double getMaxSpeed() = 0; // получить максимальную скорость
+    virtual double getCalibre() = 0; //размер главного калибра
+    virtual double getArmorWidth() = 0; //получить среднюю толщину брони
+    virtual double getSpeed() = 0; // получить текущую скорость; по умолчанию - ноль
+    virtual void setSpeed() = 0; //установить скорость движения; ограничена максимальной
+protected:
+    string name;
+    double weight;
+    double MaxTime;
+    double MaxSpeed;
+    double speed;
+    double armor;
+};
+
+int main()
+{
+    //тестовый код - надо придумать и написать
+    return 0;
+}
